4fbdb20a9f8107d11c425e656712fd6ff085699e
Luke Lovett <luke.lovett@mongodb.com>
2016-10-25 12:01:50 -0700

    Version -> .dev0

--- pymodm/__init__.py
+++ pymodm/__init__.py
@@ -16,4 +16,4 @@ from pymodm.base import MongoModel, EmbeddedMongoModel
 from pymodm.connection import connect
 
 
-version = '0.1.0'
+version = '0.2.dev0'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.2.dev0'
 
 LONG_DESCRIPTION = None
 try:
