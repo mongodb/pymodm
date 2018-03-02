05acb63f888640d647cb3129707d6da24c07f98a
David Percy <david.percy@mongodb.com>
2018-03-02 17:07:20 -0500

    Add test cases for connect=False

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,4 +1,6 @@
 from pymodm.connection import connect, _get_connection
+from pymodm import MongoModel, CharField
+from pymongo import IndexModel
 
 from test import ODMTestCase
 
@@ -11,3 +13,36 @@ class ConnectionTestCase(ODMTestCase):
         client = _get_connection('foo-connection').database.client
         self.assertEqual(42, client.max_pool_size)
         self.assertEqual(10, client.min_pool_size)
+
+    def test_connect_lazily(self):
+        connect('mongodb://localhost:27017/foo',
+                'foo-connection',
+                connect=False)
+        client = _get_connection('foo-connection').database.client
+
+        class Article(MongoModel):
+            title = CharField()
+            class Meta:
+                connection_alias = 'foo-connection'
+        self.assertFalse(client._topology._opened)
+
+        self.assertEqual(Article.objects.count(), 0)
+        self.assertTrue(client._topology._opened)
+
+    def test_connect_lazily_with_index(self):
+        connect('mongodb://localhost:27017/foo',
+                'foo-connection',
+                connect=False)
+        client = _get_connection('foo-connection').database.client
+
+        class Article(MongoModel):
+            title = CharField()
+            class Meta:
+                connection_alias = 'foo-connection'
+                indexes = [
+                    IndexModel([('title', 1)])
+                ]
+        self.assertFalse(client._topology._opened)
+
+        self.assertEqual(Article.objects.count(), 0)
+        self.assertTrue(client._topology._opened)
