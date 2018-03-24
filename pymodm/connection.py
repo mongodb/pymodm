b941806edd7babce3fc517851a2f38591d2db989
ericd34n <ericdeansanchez@gmail.com>
2018-03-23 20:16:31 -0700

    tracking pymodm aside from other w.i.p

--- pymodm/connection.py
+++ pymodm/connection.py
@@ -1,3 +1,4 @@
+
 # Copyright 2016 MongoDB, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +16,8 @@
 """Tools for managing connections in MongoModels."""
 import sys
 
+from collections import namedtuple
+
 from pymongo import uri_parser, MongoClient
 
 from pymodm.compat import reraise
@@ -22,70 +25,27 @@ from pymodm.compat import reraise
 
 __all__ = ['connect']
 
+
+"""Information stored with each connection alias."""
+ConnectionInfo = namedtuple(
+    'ConnectionInfo', ('parsed_uri', 'conn_string', 'database'))
+
+
 DEFAULT_CONNECTION_ALIAS = 'default'
+
 
 _CONNECTIONS = dict()
 
 
-class ConnectionInfo:
-    """Stores the information of each connection alias"""
-
-    __slots__ = ('parsed_uri', 'conn_string', 'database')
-
-    def __init__(self, parsed_uri, conn_string, database):
-        """Initializes the ConnectionInfo Object
-
-        :parameters:
-          - `parsed_uri` (dict): A dict of the form:
-
-            {
-                'nodelist': <list of (host, port) tuples>,
-                'username': <username> or None,
-                'password': <password> or None,
-                'database': <database name> or None,
-                'collection': <collection name> or None,
-                'options': <dict of MongoDB URI options>
-            }
-
-          - `connection_str` (str): expects a `mongodb_uri`, a MongoDB connection
-            of the general form:
-
-                'mongodb://localhost:27017/<db_name>'
-
-            Any options may be passed within the string that are supported by PyMongo.
-            `mongodb_uri` must specify a database, which will be used by any
-            :class:`~pymodm.MongoModel` that uses this connection.
-
-          - `database` (pymongo.database.Database): of the general form:
-
-                Database(
-                    MongoClient(
-                        host=['localhost:27017'],
-                        document_class=dict,
-                        tz_aware=False,
-                        connect=False,
-                        event_listeners=[]
-                    ),
-                    'foo'
-                )
-        """
-        self.parsed_uri = parsed_uri
-        self.conn_string = conn_string
-        self.database = database
-
-
 def connect(mongodb_uri, alias=DEFAULT_CONNECTION_ALIAS, **kwargs):
     """Register a connection to MongoDB, optionally providing a name for it.
-
     Note: :func:`connect` must be called with before any
     :class:`~pymodm.MongoModel` is used with the given `alias`.
-
     :parameters:
       - `mongodb_uri`: A MongoDB connection string. Any options may be passed
         within the string that are supported by PyMongo. `mongodb_uri` must
         specify a database, which will be used by any
         :class:`~pymodm.MongoModel` that uses this connection.
-
       - `alias`: An optional name for this connection, backed by a
         :class:`~pymongo.mongo_client.MongoClient` instance that is cached under
         this name. You can specify what connection a MongoModel uses by
@@ -94,7 +54,6 @@ def connect(mongodb_uri, alias=DEFAULT_CONNECTION_ALIAS, **kwargs):
         the :class:`~pymodm.context_managers.switch_connection` context
         manager.  Note that calling `connect()` multiple times with the same
         alias will replace any previous connections.
-
       - `kwargs`: Additional keyword arguments to pass to the underlying
         :class:`~pymongo.mongo_client.MongoClient`.
     """
